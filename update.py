f30c00b0e2fb688f2e8997d8cbfe90d53b1902fd
kavindunilshan <kavindunilshanliyanage@gmail.com>
2025-01-07 19:00:48 +0530

    Final combined dataset

---
+++ update.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv('52_week_dengue_dataset.csv')
+df2 = pd.read_csv('knn_imputed_precipitation_kriging_level_regression.csv')
+df3 = pd.read_csv('knn_imputed_precipitation_ordinary_kriging.csv')
+df4 = pd.read_csv('Br_vegetation_with_City.csv')
